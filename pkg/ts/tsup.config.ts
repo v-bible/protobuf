adedacc856606ab6480efcbd4382dca9422a2ad6
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-04-02 12:14:15 +0700

    fix: update publish ci/cd
    chore: update tsconfigs

---
+++ pkg/ts/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["lib/index.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  outDir: "dist",
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});
